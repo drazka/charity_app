c4af6287377c7b26fafe75268d929a79c457dbc3
Izabela Draga <izabdr@gmail.com>
2019-07-17 19:00:03 +0200

    form step1

--- src/main/java/pl/coderslab/charity/controller/DonationController.java
+++ src/main/java/pl/coderslab/charity/controller/DonationController.java
@@ -2,9 +2,16 @@ package pl.coderslab.charity.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.charity.entity.Category;
 import pl.coderslab.charity.repository.CategoryRepository;
 import pl.coderslab.charity.repository.DonationRepository;
 import pl.coderslab.charity.repository.InstitutionRepository;
+
+import java.util.List;
 
 @Controller
 public class DonationController {
@@ -17,4 +24,15 @@ public class DonationController {
 
     @Autowired
     InstitutionRepository institutionRepository;
+
+    @ModelAttribute("categories")
+    public List <Category> getCategories() {
+        return categoryRepository.findAll();
+    }
+
+    @GetMapping("/donation")
+    public String formGet(Model model){
+        return "form";
+    }
 }
+
